525d44020ef6c695f8066bc62c5dfcb9592b52b8
Margus Freudenthal <margus@cyber.ee>
2010-09-24 18:12:19 +0300

    Added image-loading code to templates.

--- sancta/wizard/template-empty/src/plugin/ee/cyber/simplicitas/example/ExampleConfig.scala
+++ sancta/wizard/template-empty/src/plugin/ee/cyber/simplicitas/example/ExampleConfig.scala
@@ -24,6 +24,28 @@ object #{class}Config {
     */
   val colors: Map[Symbol, Tuple3[String, String, Number]] =
     Map.empty
+
+  /** This object contains references to images that are loaded via
+    * plugin's ImageRegistry. The image objects are declared in this object
+    * and initialized in the <code>initializeImages</code> method. */
+  object Images {
+    /* var imageX: Image = null */
+  }
+
+  /** Load images that will be used as icons in the <code>treeImage</code>
+    * and <code>fileImage</code> methods.
+    * This method is passed two-argument function <code>addFun</code>
+    * that is used to actually load images. It takes two arguments.
+    * The first argument is key that is used in the plugin's ImageRegistry.
+    * (you do not need to do anything with they key except ensure that every
+    * image has unique key). The second argument is relative path to the
+    * image file. The path is relative to root of the plugin's jar.
+    * The function returns Image object that can later be used in
+    * <code>treeImage</code> and <code>fileImage</code> methods.
+    */  
+  def initializeImages(addFun: (String, String) => Image) {
+    /* Images.imageX = addFun("imageX", "icons/imageX.gif") */
+  }
 }
 
 class #{class}Config extends APluginConfig {
