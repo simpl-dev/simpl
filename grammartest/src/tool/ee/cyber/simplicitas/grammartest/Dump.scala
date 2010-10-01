b4d1d599c2d433b1379e1d7daf55dfe288d6ef6e
Margus Freudenthal <margus@cyber.ee>
2010-10-01 13:07:42 +0300

    Start of dump.

--- grammartest/src/tool/ee/cyber/simplicitas/grammartest/Dump.scala
+++ grammartest/src/tool/ee/cyber/simplicitas/grammartest/Dump.scala
@@ -3,6 +3,36 @@ package ee.cyber.simplicitas.grammartest
 import ee.cyber.simplicitas.CommonNode
 
 object Dump {
-    def dumpNode(node: CommonNode): String =
-        node.toString
+    val indent = "  "
+
+    def dumpNode(node: CommonNode): String = {
+        val buf = new StringBuilder()
+        dumpNode(buf, node, "")
+        buf.toString
+    }
+
+    def dumpNode(buf: StringBuilder, node: CommonNode, 
+            prefix: String) {
+        if (node == null)
+            return
+
+        // Write node header.
+        buf.append(prefix)
+        buf.append(node.startIndex)
+        buf.append(":")
+        buf.append(node.endIndex)
+        buf.append(":")
+        buf.append(baseName(node.getClass.getName))
+        buf.append("\n")
+
+        // Write children
+    }
+
+    def baseName(s: String) = {
+        val idx = s.lastIndexOf('.')
+        if (idx == -1)
+            s
+        else
+            s.substring(idx + 1)
+    }
 }
