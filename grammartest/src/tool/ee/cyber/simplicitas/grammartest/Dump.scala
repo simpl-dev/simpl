4973fd1d2c9a0b21d7a3879699bd39f2e6565dc6
Margus Freudenthal <margus@cyber.ee>
2010-10-01 14:26:23 +0300

    Implemented the dumping function.

--- grammartest/src/tool/ee/cyber/simplicitas/grammartest/Dump.scala
+++ grammartest/src/tool/ee/cyber/simplicitas/grammartest/Dump.scala
@@ -11,21 +11,53 @@ object Dump {
         buf.toString
     }
 
-    def dumpNode(buf: StringBuilder, node: CommonNode, 
+    def dumpNode(buf: StringBuilder, obj: Any, 
             prefix: String) {
-        if (node == null)
-            return
-
+        obj match {
+            case node: CommonNode =>
+                dumpCommonNode(buf, node, prefix)
+            case null =>
+                buf.append("(null)\n")
+            case lst: List[Any] =>
+                buf.append("List {\n")
+                val newPref = prefix + indent
+                for (item <- lst) {
+                    buf.append(newPref)
+                    dumpNode(buf, item, newPref)
+                }
+                buf.append(prefix + "}\n")
+            case _ =>
+                buf.append("\"" + obj + "\"")
+                buf.append("\n")
+        }
+    }
+    
+    def dumpCommonNode(buf: StringBuilder, node: CommonNode,
+            prefix: String) {
         // Write node header.
-        buf.append(prefix)
         buf.append(node.startIndex)
         buf.append(":")
         buf.append(node.endIndex)
         buf.append(":")
         buf.append(baseName(node.getClass.getName))
-        buf.append("\n")
+        buf.append(" {\n")
 
-        // Write children
+        // Create list of pairs (name, value)
+        val children = node.childrenNames.zipWithIndex.map(
+                arg =>
+                    (arg._1, node.productElement(arg._2)))
+        // Sort the children so they always will appear
+        // in the same order.
+        val sorted = children.sortWith(_._1 < _._1)
+
+        val newPref = prefix + indent
+        for ((name, value) <- sorted) {
+            buf.append(newPref)
+            buf.append(name)
+            buf.append("=")
+            dumpNode(buf, value, newPref)
+        }
+        buf.append(prefix + "}\n")
     }
 
     def baseName(s: String) = {
