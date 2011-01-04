9797dfcc9bac8424cca1f3a698fea2797703ae4b
Margus Freudenthal <margus@cyber.ee>
2011-01-04 16:52:14 +0200

    Make the binary operators work.

--- grammartest/src/tool/ee/cyber/simplicitas/grammartest/PufExtras.scala
+++ grammartest/src/tool/ee/cyber/simplicitas/grammartest/PufExtras.scala
@@ -6,35 +6,83 @@ case class Unary(op: UnaryOp.Type, arg: Expr) extends Expr {
     def childrenNames = Array("op", "arg")
 }
 
+case class Binary(op: BinaryOp.Type, left: Expr, right: Expr) extends Expr {
+    def childrenNames = Array("op", "left", "right")
+}
+
+case class ListNil extends Expr {
+    def childrenNames = Array.empty
+}
+
 object PufExtras {
-    def makeBinaryOp(op: TerminalNode, left: Expr, right: Expr) = {
-        left
+    type WithText = {
+        def text: String
     }
 
-    def makeBinaryOp(args: List[Expr], ops: List[CommonNode]) = {
-        args(0)
+    def makeBinaryOp(op: TerminalNode, left: Expr, right: Expr) =
+        if (right ne null)
+            Binary(BinaryOp.withName(op.text), left, right)
+        else
+            left
+
+    def makeBinaryOp(args: List[Expr], ops: List[WithText]) = {
+        def loop(left: Expr, right: List[Expr],
+                 ops: List[BinaryOp.Type]): Expr =
+            (right, ops) match {
+                case (rh :: rt, oh :: ot) =>
+                    loop(Binary(oh, left, rh), rt, ot)
+                case (Nil, Nil) =>
+                    left
+            }
+
+        loop(args.head, args.tail,
+            ops.map((op: WithText) => BinaryOp.withName(op.text)))
     }
 
     def makeBinaryOp(op: BinaryOp.Type, args: List[Expr]) = {
-        new Num("42")
+        splitLeftAssoc(args, op)
     }
 
-    def makeBinaryOp(op: BinaryOp.Type, left: Expr, right: Expr) = {
-        new Num("42")
+    def makeBinaryOp(op: BinaryOp.Type, left: Expr, right: Expr) =
+        if (right ne null)
+            Binary(op, left, right)
+        else
+            left
+
+    implicit def binaryCons(op: BinaryOp.Type):
+            (Expr, Expr) => Expr =
+        Binary(op, _, _)
+
+    def splitLeftAssoc(lst: List[Expr], cons: (Expr, Expr) => Expr): Expr = {
+        lst match {
+            case h :: t =>
+                t.foldLeft(h)(cons)
+            case Nil =>
+                throw new Exception("Invalid node: " + lst)
+        }
     }
 }
 
 object BinaryOp extends Enumeration {
     type Type = Value
 
-    val Cons = Value
-    val And = Value
-    val Or = Value
+    val Plus = Value("+")
+    val Minus = Value("-")
+    val Times = Value("*")
+    val Div = Value("/")
+    val Mod = Value("%")
+    val LessThan = Value("<")
+    val LessEqual = Value("<=")
+    val GreaterThan = Value(">")
+    val GreaterEqual = Value(">=")
+    val Equals = Value("==")
+    val NotEquals = Value("/=")
+    val And = Value("&&")
+    val Or = Value("||")
 }
 
 object UnaryOp extends Enumeration {
     type Type = Value
 
-    val Not = Value
-    val Neg = Value
+    val Neg, Not = Value
 }
