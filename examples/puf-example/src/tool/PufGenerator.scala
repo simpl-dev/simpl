21e279aa91f8f76f114f510bd2ef405b8528a56e
Margus Freudenthal <margus@cyber.ee>
2011-01-26 13:10:46 +0200

    Show labels for outline view.

--- examples/puf-example/src/tool/PufGenerator.scala
+++ examples/puf-example/src/tool/PufGenerator.scala
@@ -3,22 +3,22 @@ package ee.cyber.simplicitas.puf;
 import ee.cyber.simplicitas.{GeneratorBase, MainBase}
 import ee.cyber.simplicitas.PrettyPrint._
 
-class PufGenerator(destDir: String) 
+class PufGenerator(destDir: String)
         extends GeneratorBase(destDir) {
-  def generate(tree: Program) {
-      println(prettyPrint(tree))
-  }
-}
-  
-object PufMain extends MainBase {
-  def main(argv: Array[String]) {
-    parseOptions(argv)
-    val grammar = new PufGrammar()
-    for (arg <- sources) {
-      grammar.parseFile(arg)
-      checkErrors(grammar.errors)
-      
-      new PufGenerator(destDir).generate(grammar.tree)        
+    def generate(tree: Program) {
+        println(prettyPrint(tree))
     }
-  }
+}
+
+object PufMain extends MainBase {
+    def main(argv: Array[String]) {
+        parseOptions(argv)
+        val grammar = new PufGrammar()
+        for (arg <- sources) {
+            grammar.parseFile(arg)
+            checkErrors(grammar.errors)
+
+            new PufGenerator(destDir).generate(grammar.tree)
+        }
+    }
 }
