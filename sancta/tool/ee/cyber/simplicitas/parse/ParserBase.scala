83a0f4ace596937de4c0afb23f890f953ab76024
Margus Freudenthal <margus@cyber.ee>
2011-02-28 15:03:38 +0200

    Copypasted getErrorMessage method from ANTLR sources.

--- sancta/tool/ee/cyber/simplicitas/parse/ParserBase.scala
+++ sancta/tool/ee/cyber/simplicitas/parse/ParserBase.scala
@@ -2,10 +2,8 @@
 
 package ee.cyber.simplicitas.parse
 
-import collection.mutable.ArrayBuffer
 import ee.cyber.simplicitas.CommonNode
-import org.antlr.runtime.{Token, TokenStream, RecognizerSharedState,
-                          RecognitionException}
+import org.antlr.runtime._
 
 /** Base class for generated ANTLR parsers. */
 class ParserBase(input: TokenStream, state: RecognizerSharedState)
@@ -46,5 +44,32 @@ class ParserBase(input: TokenStream, state: RecognizerSharedState)
     override def displayRecognitionError(tokenNames: Array[String],
                                          ex: RecognitionException) {
         emitErrorMessage(getErrorMessage(ex, tokenNames))
+    }
+
+    override def getErrorMessage(e: RecognitionException,
+                                 tokenNames: Array[String]): String = {
+        def tokenName(expecting: Int) =
+            if (expecting== Token.EOF)
+                "EOF";
+            else
+                tokenNames(expecting)
+
+        e match {
+            case ute: UnwantedTokenException =>
+                "extraneous input " +
+                        getTokenErrorDisplay(ute.getUnexpectedToken()) +
+                        " expecting " + tokenName(ute.expecting)
+            case mte: MissingTokenException =>
+                "missing " + tokenName(mte.expecting) + " at " +
+                        getTokenErrorDisplay(e.token)
+            case mte: MismatchedTokenException =>
+                "mismatched input " + getTokenErrorDisplay(e.token) +
+                        " expecting " + tokenName(mte.expecting)
+            case mtne: MismatchedTreeNodeException =>
+                "mismatched tree node: " + mtne.node +
+                        " expecting " + tokenName(mtne.expecting)
+            case _ =>
+                super.getErrorMessage(e, tokenNames)
+        }
     }
 }
