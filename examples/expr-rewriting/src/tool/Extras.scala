416fc6f4f5dc052a22c80b0d932a8841deb735a3
Margus Freudenthal <margus@cyber.ee>
2012-09-30 15:20:54 +0300

    Make operators left-associative to get correct evaluation results.

--- examples/expr-rewriting/src/tool/Extras.scala
+++ examples/expr-rewriting/src/tool/Extras.scala
@@ -8,9 +8,16 @@ case class BinOp(left: Expr, op: String, right: Expr)
 }
 
 object Extras {
-    def makeExpr(left: Expr, op: TerminalNode, right: Expr) =
-        if (right eq null)
-            left
-        else
-            BinOp(left, op.text, right)
+    def makeExpr(ops: List[TerminalNode], args: List[Expr]): Expr =
+        args match {
+            case h :: Nil =>
+                h
+            case l :: r :: t =>
+                makeExpr(
+                    ops.tail,
+                    BinOp(l, ops.head.text, r) :: t)
+            case _ =>
+                // Just to make warning go away.
+                throw new IllegalArgumentException();
+        }
 }
